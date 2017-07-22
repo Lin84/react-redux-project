d12ec83f7419466e6b0c00b4d1f948a96320a878
Linh Le Tuan <linh.tuan@actum.cz>
2017-07-22 22:28:35 +0200

    add loading spinner

---
+++ src/app/utilities/loadingCircle.js
@@ -0,0 +1,23 @@
+function createLoadingCircle() {
+    if (!document.querySelector('.loader__overlay')) {
+        const loadingCircleHtml = document.createElement('div');
+        loadingCircleHtml.className = 'loader__overlay';
+        loadingCircleHtml.innerHTML = '<div class="loader__container"><div class="loader loader__circle"></div></div>';
+        document.body.appendChild(loadingCircleHtml);
+    }
+}
+
+function removeLoadingCircle() {
+    if (document.querySelector('.loader__overlay')) {
+        document.querySelector('.loader__overlay').classList.remove('js-loader-active');
+        document.body.removeChild(document.querySelector('.loader__overlay'));
+    }
+}
+
+function showLoadingCircle() {
+    if (document.querySelector('.loader__overlay')) {
+        document.querySelector('.loader__overlay').classList.add('js-loader-active');
+    }
+}
+
+export { createLoadingCircle, removeLoadingCircle, showLoadingCircle };
