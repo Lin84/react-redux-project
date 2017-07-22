246946ac326fd46fc2c051193c771078f2fe494b
Linh Le Tuan <linh.tuan@actum.cz>
2017-07-22 20:53:06 +0200

    clear unused javascript

--- src/app/Reducers/root-reducer.js
+++ src/app/Reducers/root-reducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-// import counter from '../components/plus-one/reducer';
-// import cardForm from './cardForm';
 import data from './data';
+import handleSubmitError from './handleSubmitError';
 
 const rootReducer = combineReducers({
-    // counter,
-    // cardForm,
+    handleSubmitError,
     data
 });
 
